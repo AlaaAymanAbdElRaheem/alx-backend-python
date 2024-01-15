afd2acec1cb8c8a153b0a0a2cdacb6debd7861c0
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2024-01-15 17:58:16 +0200

    task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -16,4 +16,4 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     delays: List[float] = []
     for i in range(n):
         delays.append(wait_random(max_delay))
-    return await asyncio.gather(*delays)
+    return sorted(await asyncio.gather(*delays))
