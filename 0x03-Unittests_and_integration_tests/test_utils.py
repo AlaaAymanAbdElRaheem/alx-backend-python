60b010afaeb67cacd21d8240f2d616a27c396849
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2024-02-04 19:11:32 +0200

    task 0

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""unit test for utils"""
+
+
+import unittest
+from unittest.mock import patch, Mock
+from parameterized import parameterized
+from utils import (
+    access_nested_map,
+    get_json,
+    memoize,
+)
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """unit test for utils.access_nested_map"""
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Test access_nested_map with different inputs"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
